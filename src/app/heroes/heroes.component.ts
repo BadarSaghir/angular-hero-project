36dc3ffd96773643f30d5e15af932e157666c58f
BadarSaghir <badardani11@gmail.com>
2022-03-27 14:00:23 +0500

    Hero project

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+import { HEROES } from '../mock-hero';
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+
+  heroes: Hero[] =[]
+  selectedHero?: Hero;
+
+
+
+
+  constructor( private heroService: HeroService, private messageService: MessageService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+  private getHeroes() {
+    this.heroService.getHeroes().subscribe(
+      heros => this.heroes = heros
+    );
+  }
+
+  // onSelect(hero:Hero):void{
+onSelect(hero: Hero): void {
+  console.log(hero)
+  this.selectedHero =  hero;
+  this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+
+}
+  // }
+
+}
+
+
